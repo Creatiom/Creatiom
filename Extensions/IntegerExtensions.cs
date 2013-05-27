76691ad8e698d84175d75872589cecf574738c15
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-05-27 12:58:41 -0400

    Initial commit

---
+++ Extensions/IntegerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Return the same value. Only added to add more specificity or readability. 2.Pixels.
+        /// </summary>
+        /// <param name="i">Give I</param>
+        /// <returns>Returns I</returns>
+        public static Int32 Characters(this int i)
+        {
+            return i;
+        }
+    }
+}
